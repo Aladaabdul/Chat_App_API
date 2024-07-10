7ee5c7cf5c3e1ccc0bc86d7e93d9f768ea542e07
aladaabdul <aladarahman18@gmail.com>
2024-07-10 17:29:37 +0100

    websocket

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const http = require("http");
 const app = require("./app");
-const {initializeWebSocket} = require("./utils/websocket")
-const PORT = process.env.PORT || 8000;
+const { initializeWebSocket } = require("./utils/websocket")
 
 
 const server = http.createServer(app);
 
 initializeWebSocket(server);
 
+const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- utils/websocket.js
+++ utils/websocket.js
@@ -1,31 +1,53 @@
-const socketIo = require("socket.io");
-const Message = require("../model/Message")
+const socketIo = require('socket.io');
+const Message = require('../model/Message');
 
 let io;
 
 function initializeWebSocket(server) {
-    io = socketIo(server);
+    io = socketIo(server, {
+        cors: {
+            origin: '*',
+            methods: ['GET', 'POST']
+        }
+    });
 
     io.on('connection', (socket) => {
         console.log("A user connected");
 
-        socket.on('disconnect', () => {
-            console.log("User disconnected")
+         socket.on('message', (message) => {
+            console.log(message);
+            let parsedMessage;
+            try {
+                parsedMessage = JSON.parse(message);
+            } catch (error) {
+                console.error('Error parsing message:', error);
+                return;
+            }
+            if (parsedMessage.event === 'join') {
+                const userId = parsedMessage.data;
+                console.log(`User ${userId} joined room ${userId}`);
+                socket.join(userId);
+            } else if (parsedMessage.event === 'chat message') {
+                const msg = parsedMessage.data;
+                console.log("Message received:", msg);
+
+                const newMessage = new Message({
+                    sender: msg.sender,
+                    receiver: msg.receiver,
+                    content: msg.content
+                });
+                newMessage.save().then(() => {
+                    // Emit the message to the recipient's room
+                    io.to(msg.receiver).emit('chat message', msg);
+                }).catch((err) => console.error(err));
+            }
         });
 
-        socket.on("chat message", async(msg) => {
-            console.log("Message received:", msg);
 
-            const newMessage = new Message({
-                sender: msg.sender,
-                receiver: msg.receiver,
-                content: msg.content
-            });
-            await msg.save();
-
-            io.emit('chat message', msg);
-        })
-    })
+        socket.on('disconnect', () => {
+            console.log("User disconnected");
+        });
+    });
 }
 
 module.exports = {
